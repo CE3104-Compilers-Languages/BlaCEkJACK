71df9c00a4ca9d639f90bb3cadd102a76727a365
Juan <juanpo.alvarado@gmail.com>
2019-10-16 16:49:51 -0600

    Archivos separados de lógica e interfaz

--- logica.rkt
+++ logica.rkt
@@ -1,1 +1,51 @@
 #lang racket
+
+
+(define (combinar-aux Lista1 Lista2 valores) (
+                                  cond ((null? Lista2)
+                                        '())
+                                       (else
+                                        (append
+                                         (list (list (car valores ) (car Lista2) (car Lista1)))
+                                         (combinar-aux Lista1 (cdr Lista2) (cdr valores))))
+                                       ))
+
+(define (combinar Lista1 Lista2 valores) (
+                                  cond ((null? Lista1)
+                                        '())
+                                       (else
+                                        (append (combinar-aux Lista1 Lista2 valores) (combinar (cdr Lista1) Lista2 valores)))
+                                       ))
+(provide combinar)
+
+(define (delete Lista N) (
+                    cond ((null? Lista)
+                          '())
+                         ((zero? N)
+                          (cdr Lista))
+                         (else
+                          (cons (car Lista) (delete (cdr Lista) (- N 1))))
+                         ))
+
+(provide delete)
+
+(define (get Lista N) (
+                    cond ((null? Lista)
+                          #f)
+                         ((zero? N)
+                          (car Lista))
+                         (else
+                          (get (cdr Lista) (- N 1)))
+                         ))
+
+  
+(define (pedir-aux Njugador jugadores carta mazo) (
+                          cond ((zero? Njugador)
+                                (cons (list (caar jugadores)
+                                      (append (cadar jugadores) (list (get mazo carta))))
+                                      (cdr jugadores)))
+                               (else
+                                (cons (car jugadores) (pedir-aux (- Njugador 1) (cdr jugadores) carta mazo)))
+                               ))
+(provide pedir-aux)
+
