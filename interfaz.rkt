4aa12a6f44dfe0d60720fc647e8a4b7e9a5f4fd9
Juan <juanpo.alvarado@gmail.com>
2019-10-19 11:26:15 -0600

    Merge manual

--- interfaz.rkt
+++ interfaz.rkt
@@ -31,6 +31,11 @@
   (set! CantidadJugadores (+ 1 (len nombres)))
   )
 
+(define (IniciarJuego nombres)
+  (GenerarJugadores nombres)
+  (IniciarCartas (* 2 CantidadJugadores))
+  )
+
 ;Método que actualiza los mazos de la partida al pedir una nueva carta
 (define (pedir Njugador)
   (let* ([rnd (random conteo)] [carta (get mazo rnd)])
@@ -41,12 +46,12 @@
                                )
 
 ;Método que pide un número N de cartas que se distribuyen entre todos los jugadores
-(define (iniciar Ncartas) (
+(define (IniciarCartas Ncartas) (
                            cond ((zero? Ncartas)
                                  )
                                 (else
                                  (pedir (remainder Ncartas CantidadJugadores))
-                                 (iniciar (- Ncartas 1)))
+                                 (IniciarCartas (- Ncartas 1)))
                                 ))
 ;Método que finaliza el turno de un jugador y pasa al siguiente al siguiente
 (define (plantar)
@@ -54,7 +59,6 @@
   turno
   )
 ;
-(define (suma Njugador) 1)
 
 ;Método que evalúa si las cartas del crupier suman 16 o menos
 ;de ser así pide una carta más para el crupier y devuelve un True
@@ -66,5 +70,12 @@
                              (else
                               #f)
                              ))
+
+
+(define (suma njugador)
+  (sumar_jugador_aux njugador jugadores 0)
+  )
+
+
 
 
